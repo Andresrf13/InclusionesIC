22995ebc6998dcac4648af7b5b8ce88b5c657bda
Andresrf13 <andres.rf13@outlook.com>
2014-08-28 01:08:43 -0600

    IniciarProyecto

---
+++ Inclusiones IC Web/Comite.Master.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace InclusionesIC_Proyecto {
+    
+    
+    public partial class Comite {
+        
+        /// <summary>
+        /// Control head.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control btnIniciaSesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIniciaSesion;
+        
+        /// <summary>
+        /// Control ContentPlaceHolder1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    }
+}
---
+++ Inclusiones IC Web/ModuloEstudiante/Inicio.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Estudiantes.Master" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="InclusionesIC_Proyecto.ModuloEstudiante.Inicio" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <h1>¡Inclusiones IC!
+                </h1>
+                <p>El sistema de inclusiones IC le permitirá dar seguimiento al proceso de inclusiones de la escuela de ingeniería en Computación. </p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6" style=" text-align: center">
+                    <div class="btn-group-vertical">
+                        <h2 >Solicitar Inclusión</h2>
+                        <asp:ImageButton runat="server" ImageUrl="../Imagenes/registro.png" CssClass="img-responsive" ></asp:ImageButton>
+                    </div>
+                    
+                    
+                </div>
+                <div class="col-md-6" style=" text-align: center">
+                    <div class="btn-group-vertical">
+                        <h2 >Consultar Inclusiones</h2>
+                        <asp:ImageButton runat="server" ImageUrl="../Imagenes/Lupa.jpg" CssClass="img-responsive" ></asp:ImageButton>
+                    </div>
+            </div>
+
+        </div>
+    </div>
+</asp:Content>
