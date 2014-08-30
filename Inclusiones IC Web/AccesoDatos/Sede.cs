99f119de8b99f89b753f6a66ccfad2aca5f3e277
Andresrf13 <andres.rf13@outlook.com>
2014-08-30 17:57:03 -0600

    Web Config y Prueba Base de datos

---
+++ Inclusiones IC Web/AccesoDatos/Sede.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Inclusiones_IC_Web.AccesoDatos
+{
+    public class Sede
+    {
+        SqlConnection conexion;
+        private void Conectar()
+        {
+            string strCon = System.Configuration.ConfigurationManager.ConnectionStrings["dbInclusionesIC"].ConnectionString;
+            conexion = new SqlConnection();
+            conexion.ConnectionString = strCon;
+        }
+
+
+        public DataTable SeleccionarTodos()
+        {
+            DataTable dtsedes = null;
+            try
+            {
+                Conectar();
+                conexion.Open();
+                string queryString = "select * from Sede";
+                SqlDataAdapter adapter = new SqlDataAdapter(queryString, conexion);
+                DataSet _datos = new DataSet();
+                adapter.Fill(_datos, "Inclusiones");
+                dtsedes = _datos.Tables["Inclusiones"];
+            }
+            catch (Exception e)
+            {
+                ;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            return dtsedes;
+        }
+    }
+}
--- Inclusiones IC Web/ModuloComite/OfertaAcademica.aspx.cs
+++ Inclusiones IC Web/ModuloComite/OfertaAcademica.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Inclusiones_IC_Web.AccesoDatos;
+using System.Data;           
 
 namespace Inclusiones_IC_Web.ModuloComite
 {
@@ -11,7 +13,22 @@ namespace Inclusiones_IC_Web.ModuloComite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
 
+            }
+            cargarSedes();
+        }
+
+        private void cargarSedes()
+        {
+            drpSede.Items.Clear();
+            Sede _sedes = new Sede();
+            DataTable _dt_Sedes = _sedes.SeleccionarTodos();
+            drpSede.DataSource = _dt_Sedes;
+            drpSede.DataValueField = "idSede";
+            drpSede.DataTextField = "Nombre";
+            drpSede.DataBind();
         }
     }
 }
